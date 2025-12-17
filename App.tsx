2b0a250e5f2b61fcbbba9694a3e9480cbc81fec7
Tejas <tejasdonkari@gmail.com>
2025-12-17 13:35:16 +0530

    changes in app.tsx

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Skills from "./components/Skills";
 import Experience from "./components/Experience";
